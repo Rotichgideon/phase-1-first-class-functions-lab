9f853e34598b2764f7e0ee8d81b10fc96a39c976
gideon rotich <gideonrotich2001@gmail.com>
2024-04-05 00:24:43 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+// Code your solution in this file!
+
+const returnFirstTwoDrivers = (newArr) => {
+    return newArr.slice(0, 2);
+}
+
+const returnLastTwoDrivers = (newArr) => {
+    return newArr.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (fare) => {
+    return function (value) {
+        return fare * value;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, selectingDrivers) => {
+    return selectingDrivers(drivers);
+}
